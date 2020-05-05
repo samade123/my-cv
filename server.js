0fe9f30c332d73651d87c181547979ba7fbeeb41
Samuel <samuel.ademola43@gmail.com>
2020-05-05 23:21:01 +0100

    added login pages

--- server.js
+++ server.js
@@ -12,18 +12,41 @@ app.use(
   express.static("public")
 ); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
 
+app.use(
+  "./dist/auth",
+  express.static("public-two")
+); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
 app.use("/css", express.static(__dirname + "/public/css"));
 app.use("/js", express.static(__dirname + "/public/js"));
 app.use("/images", express.static(__dirname + "/public/images"));
 app.use("/fonts", express.static(__dirname + "/public/fonts"));
 
 app.listen(3000, "127.0.0.1", function() {
-// app.listen("80.192.157.49", function() {
+  // app.listen("80.192.157.49", function() {
   console.log("Listening on port 3000!");
 });
 
-app.get("/", function(req, res) {
+app.get("/main", function(req, res) {
   res.sendFile("dist/index.html", {
+    root: __dirname
+  });
+});
+
+
+app.get("/", function(req, res) {
+  res.redirect('/login')
+});
+
+
+app.get("/login", function(req, res) {
+  res.sendFile("dist/auth/index.html", {
+    root: __dirname
+  });
+});
+
+app.get("/login/css", function(req, res) {
+  res.sendFile("dist/auth/auth.css", {
     root: __dirname
   });
 });
@@ -63,6 +86,8 @@ app.get("/fonts/fa-brands-400.woff2", (req, res) => {
   });
 });
 
+
+
 app.get("/favicon.ico", (req, res) => {
   res.sendFile("dist/site/images/favicon.ico", {
     root: __dirname
@@ -70,8 +95,8 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 app.get("/img/qrSvg", (req, res) => {
-  console.log(req.headers.host);
-  console.log(req.headers.referer);
+  // console.log(req.headers.host);
+  // console.log(req.headers.referer);
   generateQr(req.headers.referer);
   setTimeout(function() {
     res.sendFile("dist/site/images/qr.svg", {
@@ -94,15 +119,47 @@ async () => {
 };
 // console.log(files)
 
+app.get("/security/text", (req, res) => {
+  res.sendFile("node_modules/text-security/text-security.css", {
+    root: __dirname
+  });
+});
+
+var secure = fs.readdirSync("./node_modules/text-security/")
+console.log(secure)
+secure.forEach(file => {
+  console.log(file)
+  app.get(`/text-security/${file}`, (req, res) => {
+    res.sendFile(`node_modules/text-security//${file}`, {
+      root: __dirname
+    });
+  });
+});
+
+
+// async () => {
+//   var secure = await fs.readdirSync("./dist/auth/text-security/").then(() => {
+//     secure.forEach(file => {
+//       // console.log(file)
+//       app.get(`/text-security/${file}`, (req, res) => {
+//         res.sendFile(`node_modules/text-security/${file}`, {
+//           root: __dirname
+//         });
+//       });
+//     });
+//   });
+// };
+
+
 var files = fs.readdirSync("./dist/site/images")
 files.forEach(image => {
-  console.log(image)
+  // console.log(image)
   app.get(`/images/${image}`, (req, res) => {
-    console.log(image);
-    console.log("get")
-      res.sendFile(`dist/site/images/${image}`, {
-        root: __dirname
-      });
+    // console.log(image);
+    // console.log("get")
+    res.sendFile(`dist/site/images/${image}`, {
+      root: __dirname
+    });
   });
 });
 
